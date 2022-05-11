6b533af4a5635433647dd273354b38bdc716109c
HadjSalemMohamedHedi <57866953+HadjSalemMohamedHedi@users.noreply.github.com>
2022-05-11 17:25:56 +0100

    Relation

---
+++ my-books/Controllers/BooksController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using my_books.Data.Services;
+using my_books.Data.ViewModels;
+
+namespace my_books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public BooksService _booksService;
+
+        public BooksController(BooksService BooksService)
+        {
+            _booksService= BooksService;
+        }
+
+
+
+        [HttpPost("add-book")]
+        public IActionResult AddBook([FromBody] BookVM book)
+        {
+            _booksService.AddBook(book);
+            return Ok();
+        }
+   
+    
+        [HttpGet]
+        public IActionResult GetAllBook()
+        {
+            var allBooks = _booksService.GetAllBook();
+            return Ok(allBooks);
+        }
+
+
+
+        [HttpGet("get-book-by-id/{BookId}")]
+        public IActionResult GetBookById(int BookId)
+        {
+            var Book = _booksService.GetBookById(BookId);
+            return Ok(Book);
+        }
+
+
+
+        [HttpPut("update-book/{id}")]
+        public IActionResult UpdateBookById(int id,[FromBody] BookVM book)
+        {
+            _booksService.UpdateBookById(id,book);
+            return Ok();
+        }
+
+
+        [HttpDelete("delete-book/{id}")]
+        public IActionResult DeletingBookByid(int id)
+        {
+            _booksService.DeletingBookByid(id);
+            return Ok();
+        }
+
+
+
+
+    }
+}
---
+++ my-books/Data/Services/BooksService.cs
@@ -0,0 +1,83 @@
+﻿using my_books.Data.Models;
+using my_books.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace my_books.Data.Services
+{
+    public class BooksService
+    {
+        private AppDbContext _context;
+        public BooksService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddBook(BookVM book)
+        {
+            var _book = new Book()
+            {
+                Title = book.Title,
+                Description = book.Description,
+                IsRead = book.IsRead,
+                DateRead = book.DateRead.Value,
+                Rate = book.Rate,
+                Genre = book.Genre,
+                CoverUrl = book.CoverUrl,
+                Author = book.Author,
+                DateAdded = DateTime.Now
+            };
+            _context.Books.Add(_book);
+            _context.SaveChanges();
+        }
+
+
+        public List<Book> GetAllBook()
+        {
+            var allBooks = _context.Books.ToList();
+            return allBooks;
+        }
+
+        public Book GetBookById(int idBook)
+        {
+            return _context.Books.FirstOrDefault(Book => Book.Id == idBook);
+
+        }
+
+
+        public Book UpdateBookById(int BookId, BookVM book)
+        {
+            var _book = _context.Books.FirstOrDefault(n => n.Id == BookId);
+            if (_book != null)
+            {
+                _book.Title = book.Title;
+                _book.Description = book.Description;
+                _book.IsRead = book.IsRead;
+                _book.DateRead = book.DateRead.Value;
+                _book.Rate = book.Rate;
+                _book.Genre = book.Genre;
+                _book.CoverUrl = book.CoverUrl;
+                _book.Author = book.Author;
+                _context.SaveChanges();
+            };
+            return _book;
+        }
+
+
+        public bool DeletingBookByid(int id)
+        {
+            var _book = _context.Books.FirstOrDefault(_book => _book.Id == id);
+            if (_book != null)
+            {
+
+                _context.Books.Remove(_book);
+                _context.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
